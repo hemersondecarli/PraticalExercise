a27ddd7543f9b5fbe8569039f9e7e4ed6ecf9d76
Hemerson <hemersondc96@gmail.com>
2026-01-23 05:46:24 +0000

    Few coding tweaks

--- src/Main.java
+++ src/Main.java
@@ -10,17 +10,21 @@ public class Main {
 
         //Asks the user to enter up to 5 items they need to buy
         for(int i = 0; i < 5; ++i) {
+            System.out.printf("Enter item number [%d]: ", i+1);
             item.add(scanner.nextLine());
         }
         //Prints the full shopping list
         System.out.println(item);
 
         //Add a feature that lets the user search for an item and tells them if it is in the list.
+        System.out.println("Enter item name to search on the list");
         String chooseItem = scanner.nextLine();
         if (item.contains(chooseItem)) {
             System.out.print(chooseItem + " is in the list\n");
+        }else{
+            System.out.println("the item is not in the list");
         }
         //Counts how many items were entered
-        System.out.println("You have entered " + item.size() + " items");
+        System.out.println("You have " + item.size() + " items in the list");
     }
 }
