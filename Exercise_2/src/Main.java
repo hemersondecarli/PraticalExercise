2f2bf76d717ab741209539c82f83bd26afd7f31b
Hemerson <hemersondc96@gmail.com>
2026-01-24 04:36:15 +0000

    Allowing user to store up to a max of 5 contacts

--- Exercise_2/src/Main.java
+++ Exercise_2/src/Main.java
@@ -14,13 +14,16 @@ public class Main {
         System.out.print("How many contacts do you want to store?: ");
         choice = scanner.nextInt();
         scanner.nextLine();
-
+    if (choice <= 5) {
         for (int i = 0; i < choice; i++) {
-            System.out.printf("Enter contact no. %d: ", i+1);
+            System.out.printf("Enter contact no. %d: ", i + 1);
             names.add(scanner.nextLine());
             System.out.println("Enter phone no.");
             phoneNo.add(scanner.nextLine());
         }
+    }else{
+        System.out.println("You can only store up to 5 contacts");
+    }
 
         //Print all contacts in this format:
 //        1. Alice - 0851234567
