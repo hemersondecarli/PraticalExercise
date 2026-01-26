52046bad61d504a1f4df5c2110f48023d8bf7580
Hemerson <hemersondc96@gmail.com>
2026-01-26 03:31:46 +0000

    Added variable to hold max no. of students Asked user to enter no. of students (max 5) if/for logics

--- Exercise_3_StudentGradeTracker/src/Main.java
+++ Exercise_3_StudentGradeTracker/src/Main.java
@@ -1,11 +1,26 @@
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
         ArrayList<String> names = new ArrayList<>();
-        ArrayList<String> grades = new ArrayList<>();
+        ArrayList<Integer> grades = new ArrayList<>();
+        int numberOfStudents;
+        int maxNumberOfStudents = 5;
 
 
+        System.out.println("Enter how many students you would like to add: ");
+        numberOfStudents = scanner.nextInt();
+
+        if(numberOfStudents <= maxNumberOfStudents){
+            for (int i = 0; i < numberOfStudents; i++) {
+                System.out.println("Enter student name: ");
+                names.add(scanner.next());
+                System.out.println("Enter student grade: ");
+                grades.add(scanner.nextInt());
+            }
+        }
     }
 }
