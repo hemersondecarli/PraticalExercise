73cca5051ef147e796912224867eaf66bbd3ecc4
Hemerson <hemersondc96@gmail.com>
2026-01-26 07:59:09 +0000

    Student can have grades between 0 and 100 only If requirements are met, it will save student name and grande into the arrayList

--- Exercise_3_StudentGradeTracker/src/Main.java
+++ Exercise_3_StudentGradeTracker/src/Main.java
@@ -9,18 +9,34 @@ public class Main {
         ArrayList<Integer> grades = new ArrayList<>();
         int numberOfStudents;
         int maxNumberOfStudents = 5;
+        int examScore;
+        String name;
 
 
         System.out.println("Enter how many students you would like to add: ");
         numberOfStudents = scanner.nextInt();
+        scanner.nextLine();
 
         if(numberOfStudents <= maxNumberOfStudents){
             for (int i = 0; i < numberOfStudents; i++) {
-                System.out.println("Enter student name: ");
-                names.add(scanner.next());
-                System.out.println("Enter student grade: ");
-                grades.add(scanner.nextInt());
+                System.out.println("Enter Name: ");
+                name= scanner.nextLine();
+
+
+                //grades cannot be less than 0 or over 100
+                 do {
+                     System.out.println("Enter Grade for: "+name);
+                     examScore = scanner.nextInt();
+                     scanner.nextLine();
+                 } while (examScore < 0 || examScore > 100);
+
+                 //stored in arrayList
+                    names.add(name);
+                    grades.add(examScore);
+
             }
         }
+        System.out.println(names);
+        System.out.println(grades);
     }
 }
