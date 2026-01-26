8b2312ebbfeb05b1eac677257292ce012847c85b
Hemerson <hemersondc96@gmail.com>
2026-01-26 03:17:00 +0000

    Creating 2 ArrayList from student names and student grades

--- Exercise_3_StudentGradeTracker/src/Main.java
+++ Exercise_3_StudentGradeTracker/src/Main.java
@@ -1,5 +1,11 @@
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args) {
+
+        ArrayList<String> names = new ArrayList<>();
+        ArrayList<String> grades = new ArrayList<>();
+
 
     }
 }
