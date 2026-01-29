a304530a8c2d521ba021dc4b4ea30d926da8e050
Hemerson <hemersondc96@gmail.com>
2026-01-29 23:38:00 +0000

    -created a method for each type: +ROCK +PAPER +SCISSORS
    -Playing around with Random method on enums

--- Exercise_4_JoKenPo/src/Main.java
+++ Exercise_4_JoKenPo/src/Main.java
@@ -1,15 +1,18 @@
+import java.util.Arrays;
+import java.util.Random;
 import java.util.Scanner;
+
 public class Main {
     static Scanner scanner = new Scanner(System.in);
+    static Random random = new Random();
 
     public static void main(String[] args) {
-        Type type = Type.SCISSORS;
 
-        System.out.println("This is a " + type.getDescription());
+        //System.out.println("This is a " + type.getDescription());
 
-        for (Type types : Type.values()) {
-            System.out.println(types);
-        }
+//        for (Type types : Type.values()) {
+//            System.out.println(types);
+//        }
 
         conditions();
 
@@ -19,12 +22,53 @@ public class Main {
         Type scissors = Type.SCISSORS;
         String choice;
 
+        //scissors.computerChoice();
+
         System.out.println("Choose between, [rock] - [paper] - [scissors]");
         choice = scanner.nextLine();
 
+        switch (choice) {
+            case "rock": rock(); break;
+            case "paper": paper(); break;
+            case "scissors": scissors(); break;
+            default: System.out.println("Invalid choice");
+        }
+    }
+
+    public static void scissors(){
+        Type scissors = Type.SCISSORS;
         //comparing enum with a string
-        if("scissors".equalsIgnoreCase(Type.SCISSORS.getDescription())) {
-            System.out.println("worked");
+        //Scissors
+        if("scissors".equalsIgnoreCase(scissors.computerChoice())) {
+            System.out.println("It's a Draw");
+        }else if ("scissors".equalsIgnoreCase(scissors.computerChoice())) {
+            System.out.println("You Lost!");
+        }else if ("scissors".equalsIgnoreCase(scissors.computerChoice())) {
+            System.out.println("You Won!");
+        }
+    }
+
+    public static void rock(){
+        Type rock = Type.ROCK;
+
+        if("rock".equalsIgnoreCase(rock.computerChoice())) {
+            System.out.println("You Won!");
+        }else if ("rock".equalsIgnoreCase(rock.computerChoice())) {
+            System.out.println("It's a Draw");
+        }else if ("rock".equalsIgnoreCase(rock.computerChoice())) {
+            System.out.println("You Lost!");
+        }
+    }
+
+    public static void paper(){
+        Type paper = Type.PAPER;
+
+        if("paper".equalsIgnoreCase(paper.computerChoice())) {
+            System.out.println("You Lost!");
+        }else if ("paper".equalsIgnoreCase(paper.computerChoice())) {
+            System.out.println("You Won!");
+        }else if ("paper".equalsIgnoreCase(paper.computerChoice())) {
+            System.out.println("It's a Draw");
         }
     }
 }
