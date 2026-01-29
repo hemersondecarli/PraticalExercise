f1e48937835ae245f860b4ced9e7a5b871919126
Hemerson <hemersondc96@gmail.com>
2026-01-29 22:28:11 +0000

    -Created a condition method that will hold the game logic

--- Exercise_4_JoKenPo/src/Main.java
+++ Exercise_4_JoKenPo/src/Main.java
@@ -1,6 +1,30 @@
+import java.util.Scanner;
 public class Main {
+    static Scanner scanner = new Scanner(System.in);
+
     public static void main(String[] args) {
+        Type type = Type.SCISSORS;
 
+        System.out.println("This is a " + type.getDescription());
 
+        for (Type types : Type.values()) {
+            System.out.println(types);
+        }
+
+        conditions();
+
+    }
+
+    public static void conditions() {
+        Type scissors = Type.SCISSORS;
+        String choice;
+
+        System.out.println("Choose between, [rock] - [paper] - [scissors]");
+        choice = scanner.nextLine();
+
+        //comparing enum with a string
+        if("scissors".equalsIgnoreCase(Type.SCISSORS.getDescription())) {
+            System.out.println("worked");
+        }
     }
 }
